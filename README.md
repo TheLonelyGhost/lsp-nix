504b999ffe52a99ccd91abbb7778992154a2c572
David Alexander <opensource@thelonelyghost.com>
2022-01-07 17:08:07 -0500

    LOTS of new language servers, mostly from nixpkgs
    By pinning nixpkgs-unstable with this flake, we can optionally upgrade
    the language servers used, as needed, without risking overall integrity
    of the rest of the system if that specific version of nixpkgs has a bug
    in it that does not impact these programs.
    
    This makes the language servers (and associated tools) more independent
    and more likely to be upgraded when new versions of tools are released.

--- README.md
+++ README.md
@@ -3,10 +3,34 @@
 Packages included:
 
 - [`dot-language-server`](https://github.com/nikeee/dot-language-server)
-- [`stylelint-lsp`](https://github.com/bmatcuk/stylelint-lsp)
+- [`gopls`](https://pkg.go.dev/golang.org/x/tools/gopls)
+- [`nim-language-server`](https://github.com/PMunch/nimlsp)
+- [`bash-language-server`](https://github.com/bash-lsp/bash-language-server)
+- [`diagnostic-language-server`](https://github.com/iamcco/diagnostic-languageserver)
+- [`dockerfile-language-server`](https://github.com/rcjsuen/dockerfile-language-server-nodejs)
+- [`pyright`](https://github.com/Microsoft/pyright)
+- [`nix-language-server`](https://github.com/nix-community/rnix-lsp)
+- [`rust-analyzer`](https://rust-analyzer.github.io/)
+- [`solargraph`](https://solargraph.org/)
+- [`stylelint-language-server`](https://github.com/bmatcuk/stylelint-lsp)
+- [`terraform-language-server`](https://github.com/hashicorp/terraform-ls)
 - [`typescript-language-server`](https://github.com/typescript-language-server/typescript-language-server)
 - [`vim-language-server`](https://github.com/iamcco/vim-language-server)
 - [`vscode-langservers-extracted`](https://github.com/hrsh7th/vscode-langservers-extracted)
+- [`yaml-language-server`](https://github.com/redhat-developer/yaml-language-server)
+
+CLI tools (to be used with `diagnostic-language-server`):
+
+- [`eslint`](https://eslint.org/)
+- [`htmlhint`](https://htmlhint.com/)
+- [`jsonlint`](https://jsonlint.com/)
+- [`stylelint`](https://stylelint.io/)
+- [`isort`](https://pycqa.github.io/isort/)
+- [`flake8`](https://flake8.pycqa.org/en/latest/)
+- [`flake8-import-order`](https://github.com/PyCQA/flake8-import-order)
+- [`yamllint`](http://www.yamllint.com/)
+- [`shellcheck`](https://www.shellcheck.net/)
+- [`vint`](https://github.com/Vimjas/vint)
 
 ## Usage
 
--- flake.nix
+++ flake.nix
@@ -33,11 +33,36 @@
           };
 
           packages = {
+            # Managed in-repo
             dot-language-server = import ./packages/dot-language-server.nix { inherit pkgs nodejs; };
             stylelint-lsp = import ./packages/stylelint-lsp.nix { inherit pkgs nodejs; };
             typescript-language-server = import ./packages/typescript-language-server.nix { inherit pkgs nodejs; };
             vim-language-server = import ./packages/vim-language-server.nix { inherit pkgs nodejs; };
             vscode-langservers-extracted = import ./packages/vscode-langservers-extracted.nix { inherit pkgs nodejs; };
+
+            # Shim so we can safely version what's stable from nixpkgs
+            eslint = pkgs.nodePackages.eslint;
+            htmlhint = pkgs.nodePackages.htmlhint;
+            jsonlint = pkgs.nodePackages.jsonlint;
+            stylelint = pkgs.nodePackages.stylelint;
+            isort = pkgs.python3Packages.isort;
+            flake8 = pkgs.python3Packages.flake8;
+            flake8-import-order = pkgs.python3Packages.flake8-import-order;
+            yamllint = pkgs.python3Packages.yamllint;
+            shellcheck = pkgs.shellcheck;
+            vint = pkgs.vim-vint;
+
+            gopls = pkgs.gopls;
+            nim-language-server = pkgs.nimlsp;
+            bash-language-server = pkgs.nodePackages.bash-language-server;
+            diagnostic-language-server = pkgs.nodePackages.diagnostic-languageserver;
+            dockerfile-language-server = pkgs.nodePackages.dockerfile-language-server-nodejs;
+            pyright = pkgs.nodePackages.pyright;
+            yaml-language-server = pkgs.nodePackages.yaml-language-server;
+            nix-language-server = pkgs.rnix-lsp;
+            rust-analyzer = pkgs.rust-analyzer-unwrapped;
+            solargraph = pkgs.solargraph;
+            terraform-language-server = pkgs.terraform-ls;
           };
         }
     );
